ba3ab280a067ac40477663adda8bc025c8f834aa
Glenn Murray <evocativeu@icloud.com>
2021-07-24 13:37:12 -0700

    reposition bio

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -33,10 +33,8 @@ const Header = ({ siteTitle }) => (
               </Link>
             </nav>
           </header>
+          <Bio />
         </Col>
-        <Col  xs={4} sm={4} md={4} lg={4} xl={4}>  
-            <Bio />
-          </Col>
     
       </Row>
     </Container>
